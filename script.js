c98ddabfb33d65fb80388dc74cf1a3feba12f257
Lewis Potticary <lewispotticary@gmail.com>
2021-07-26 22:30:05 +0100

    Append API data to HTML

--- script.js
+++ script.js
@@ -44,9 +44,58 @@ function addCrypto (clicked_id){
     !async function(){
         const res = await fetch(`https://api.coingecko.com/api/v3/coins/${clicked_id}`);
         const coin = await res.json();
-        console.log(coin);
-    }();
 
+        console.log(coin);
+
+        var coinRank = coin.coingecko_rank;
+
+        var coinLogo = coin.image.small;
+        var coinName = coin.name;
+        var coinSymbol = coin.symbol;
+        coinSymbol = coinSymbol.toUpperCase();
+
+        var coinPrice = coin.market_data.current_price.gbp;
+        var coinChange24h = coin.market_data.price_change_percentage_24h;
+        coinChange24h = Math.round(coinChange24h * 10) / 10
+
+        var portfolioContainer = document.createElement('div');
+        portfolioContainer.setAttribute('id','portfolioContainer');
+
+        var portfolioRow1 = document.createElement('div');
+        portfolioRow1.setAttribute('id','portfolioRow1');
+        var coinRankText = document.createElement('span');
+        coinRankText.setAttribute('id','spanRow1');
+        coinRankText.innerText = 'Rank #' + coinRank;
+        portfolioRow1.appendChild(coinRankText);
+
+        var portfolioRow2 = document.createElement('div');
+        portfolioRow2.setAttribute('id','portfolioRow2');
+        var coinLogoText = document.createElement('IMG');
+        coinLogoText.setAttribute('src', coinLogo);
+        var coinNameText = document.createElement('span');
+        coinNameText.innerText = coinName;
+        var coinSymbolText = document.createElement('span');
+        coinSymbolText.innerText = '('+ coinSymbol + ')';
+        portfolioRow2.appendChild(coinLogoText);
+        portfolioRow2.appendChild(coinNameText);
+        portfolioRow2.appendChild(coinSymbolText);
+
+
+        var portfolioRow3 = document.createElement('div');
+        portfolioRow3.setAttribute('id','portfolioRow3'); 
+        var coinPriceText = document.createElement('span');
+        coinPriceText.innerText = '£'+ coinPrice;
+        var coinChangeText = document.createElement('span');
+        coinChangeText.innerText = coinChange24h + '%';
+        portfolioRow3.appendChild(coinPriceText);
+        portfolioRow3.appendChild(coinChangeText);
+
+        portfolioContainer.appendChild(portfolioRow1);
+        portfolioContainer.appendChild(portfolioRow2);
+        portfolioContainer.appendChild(portfolioRow3);
+
+        portfolio.appendChild(portfolioContainer);
+    }();
 }
 
 searchBtn.addEventListener('click', () => searchCoins(search.value));
